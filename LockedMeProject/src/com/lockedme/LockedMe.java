8ff683e92373861ca2b24661868a4d9479343884
Bhupendra Dhakre <bhu_dhakre@hotmail.com>
2021-08-14 18:13:55 +0530

    Sprint2Day3

--- LockedMeProject/src/com/lockedme/LockedMe.java
+++ LockedMeProject/src/com/lockedme/LockedMe.java
@@ -12,26 +12,65 @@ public class LockedMe {
 	{
 
 
-		// Code for Search a file 
-		//Variable declaration 
-		String filename;
-		Scanner obj = new Scanner(System.in);
-		
-		// Get the file name from the user 
-		System.out.println("Enter File name to be delete : ");
-		filename = obj.nextLine();
-		
-		// Call delete method to delete the file.
-		boolean isFound = FileManager.searchFile(folderpath, filename);
-		
-		if(isFound)
-		{
-			System.out.println("File is Present in the folder.");
-		}
-		else
-		{
-			System.out.println("File is not present int the folder.");
-		}
+
 	}
 
+	public static void getallFiles()
+	{
+		
+		// Get list of files 
+		
+	 	   List<String> filenames = FileManager.getAllFiles(folderpath);
+			
+			for(String f:filenames)
+			{
+				System.out.println(f);
+			}
+	      
+	}
+	
+	public static void createFile()
+	{
+		
+		  // Call create file 
+		
+			//Variable Declaration
+			
+			Scanner obj = new Scanner(System.in);
+			String fileName ;
+			int linesCount;
+			List<String> content = new ArrayList<String>();
+			
+			// Read Filename from user.
+			System.out.println("Enter the File Name : ");
+			fileName = obj.nextLine();
+			
+			// Read number of lines from the user 
+			System.out.println("Enter how many lines in the files : ");
+			linesCount=Integer.parseInt(obj.nextLine());
+			
+			//Read lines from user 
+			for(int i =1; i <= linesCount ; i++)
+			{
+				System.out.println("Enter Lines "+i+":");
+				content.add(obj.nextLine());
+			}
+			
+			// Save the content into the files 
+			 
+			boolean isSaved = FileManager.createFiles(folderpath, fileName, content);
+			
+					if(isSaved) 
+					{
+						System.out.println("File and data saved Successfully.");
+					}
+					 else {
+							
+						 System.out.println("Some error occurred. Please contact admin@abc.com");
+					}
+			
+				// Close Scanner Objects 
+					obj.close();
+					
+	}
 }
